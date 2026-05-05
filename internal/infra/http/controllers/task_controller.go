0340c43a6cbb5e33522f21ee3f40a93833757b5e
H0lead <bladislav332@gmail.com>
2026-05-05 14:48:34 +0300

    upd: container, router, task controller. Add: task resource, request

--- internal/infra/http/controllers/task_controller.go
+++ internal/infra/http/controllers/task_controller.go
@@ -1,10 +1,13 @@
 package controllers
 
 import (
+	"errors"
+	"log"
 	"net/http"
 
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/app"
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/requests"
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/resources"
 )
 
@@ -21,6 +24,27 @@ func NewTaskController(ts app.TaskService) TaskController {
 func (c TaskController) Save() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		user := r.Context().Value(UserKey).(domain.User)
-		Success(w, resources.UserDto{}.DomainToDto(user))
+
+		task, err := requests.Bind(r, requests.TaskRequest{}, domain.Task{})
+		if err != nil {
+			log.Printf("TaskController.Save(requests.Bind): %s", err)
+			BadRequest(w, errors.New("invalid request body"))
+			return
+		}
+
+		task.UserId = user.Id
+		task.Status = domain.NewTaskSatus
+
+		task, err = c.taskService.Save(task)
+		if err != nil {
+			log.Printf("TaskController.Save(requests.Bind): %s", err)
+			InternalServerError(w, err)
+			return
+		}
+
+		taskDTO := resources.TaskDTO{}
+		taskDTO = taskDTO.DomainToDTO(task)
+
+		Success(w, taskDTO)
 	}
 }
---
+++ internal/infra/http/resources/task_resource.go
@@ -0,0 +1,27 @@
+package resources
+
+import (
+	"time"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+)
+
+type TaskDTO struct {
+	Id          uint64            `json:"id"`
+	UserId      uint64            `json:"userId"`
+	Description string            `json:"description,omitempty"`
+	Title       string            `json:"title"`
+	Status      domain.TaskStatus `json:"status"`
+	DeadLine    *time.Time        `json:"deadline,omitempty"`
+}
+
+func (d TaskDTO) DomainToDTO(t domain.Task) TaskDTO {
+	return TaskDTO{
+		Id:          t.Id,
+		UserId:      t.UserId,
+		Description: t.Description,
+		Title:       t.Title,
+		Status:      t.Status,
+		DeadLine:    t.DeadLine,
+	}
+}
