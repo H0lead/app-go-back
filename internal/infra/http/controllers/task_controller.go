548307500f297892e7745322bf149ae0dfaa01c4
H0lead <bladislav332@gmail.com>
2026-05-05 11:36:23 +0300

    Add: Task repository, service and controller

---
+++ internal/infra/http/controllers/task_controller.go
@@ -0,0 +1,26 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/app"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/resources"
+)
+
+type TaskController struct {
+	taskService app.TaskService
+}
+
+func NewTaskController(ts app.TaskService) TaskController {
+	return TaskController{
+		taskService: ts,
+	}
+}
+
+func (c TaskController) Save() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		user := r.Context().Value(UserKey).(domain.User)
+		Success(w, resources.UserDto{}.DomainToDto(user))
+	}
+}
